b0723c352e15cc179aa62b2fdcf4c25034021c96
Maxim <l9sik3@gmail.com>
2021-02-08 19:44:00 +0300

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,46 @@
+
+// var userName = 'Max';
+// let second = 2;
+// const pi = 3.14;
+
+// // const pi = 3.15;
+
+// console.log (pi);
+
+let arr = ['blue', 'red', 'green'];
+console.log (arr[0]);
+alert(arr[1]);
+confirm(arr[2]);
+
+prompt('Есть ли вам 18 лет?', 'Да');
+
+let incr = 10;
+console.log(incr++);
+console.log(incr);
+
+let phrase = "Hello " + "world!";
+console.log(phrase);
+
+// let answer = "hello" + 4;
+// console.log(answer);
+// console.log(typeof(answer));
+
+let answer = +"hello" + 4;
+console.log(answer);
+console.log(typeof(answer));
+
+let incr = 10;
+console.log(++incr);
+
+console.log('23'==2);
+
+
+let a = true;
+let b = true;
+let c = (a&&b);
+console.log(c);
+
+let a = false;
+let b = false;
+let c = (a||b);
+console.log(c);
