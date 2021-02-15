9c9b96c3e79fff398f3d50600226c88a093bdb9f
Maxim <l9sik3@gmail.com>
2021-02-15 20:01:10 +0300

    the first lesson is done

--- Script.js
+++ Script.js
@@ -1,1 +1,26 @@
-alert('hi')
+var money = +prompt ('Ваш бюджет на месяц?');
+var time = +prompt ('Введите дату в формате YYYY-MM-DD');
+var oneDayBudget;
+let appData = {
+  budget:money, //Бюджет
+  timeData:time, // Данные времени
+  expenses: {}, //обязательные платежи
+  optionalExpenses:{},// неоязательные расходы
+  income:[], //доп.доход
+  savings:false
+}
+
+var a = prompt('Введите обязательную статью расходов в этом месяце'),
+    b = +prompt('Во сколько обойдется?'),
+    c = prompt('Введите обязательную статью расходов в этом месяце'),
+    d = +prompt('Во сколько обойдется?');
+
+appData.expenses [a] = b;
+appData.expenses [c] = d;
+
+oneDayBudget = (money - b - d)/30;
+
+var x = oneDayBudget;
+var y = Math.round(oneDayBudget * 10) / 10; // Результат равен 7.7.
+alert ('Ваш дневной бюджет составляет - ' + (y));
+
