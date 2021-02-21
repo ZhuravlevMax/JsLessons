73cb7fdbd08ef30cb5903a00111ac060827d2823
Maxim <l9sik3@gmail.com>
2021-02-21 17:12:00 +0300

    answer on questions were added

--- Script.js
+++ Script.js
@@ -1,26 +1,67 @@
-var money = +prompt ('Ваш бюджет на месяц?');
-var time = +prompt ('Введите дату в формате YYYY-MM-DD');
-var oneDayBudget;
-let appData = {
-  budget:money, //Бюджет
-  timeData:time, // Данные времени
-  expenses: {}, //обязательные платежи
-  optionalExpenses:{},// неоязательные расходы
-  income:[], //доп.доход
-  savings:false
-}
+// var money = +prompt ('Ваш бюджет на месяц?');
+// var time = +prompt ('Введите дату в формате YYYY-MM-DD');
+// var oneDayBudget;
+// let appData = {
+//   budget:money, //Бюджет
+//   timeData:time, // Данные времени
+//   expenses: {}, //обязательные платежи
+//   optionalExpenses:{},// неоязательные расходы
+//   income:[], //доп.доход
+//   savings:false
+// };
 
-var a = prompt('Введите обязательную статью расходов в этом месяце'),
-    b = +prompt('Во сколько обойдется?'),
-    c = prompt('Введите обязательную статью расходов в этом месяце'),
-    d = +prompt('Во сколько обойдется?');
+// var a = prompt('Введите обязательную статью расходов в этом месяце'),
+//     b = +prompt('Во сколько обойдется?'),
+//     c = prompt('Введите обязательную статью расходов в этом месяце'),
+//     d = +prompt('Во сколько обойдется?');
 
-appData.expenses [a] = b;
-appData.expenses [c] = d;
+// appData.expenses [a] = b;
+// appData.expenses [c] = d;
 
-oneDayBudget = (money - b - d)/30;
+// oneDayBudget = (money - b - d)/30;
 
-var x = oneDayBudget;
-var y = Math.round(oneDayBudget * 10) / 10; // Округление
-alert ('Ваш дневной бюджет составляет - ' + (y));
+// var x = oneDayBudget;
+// var y = Math.round(oneDayBudget * 10) / 10; // Округление
+// alert ('Ваш дневной бюджет составляет - ' + (y));
+
+// let num = 50;
+// while (num < 55) {
+//   console.log(num);
+//   num++;
+// }
+
+// // let num = 53;
+// do {
+//   console.log (num);
+//   num++;
+// }
+// while (num < 55);
+
+// for ( let i = 1; i < 8; i++) {
+//   if (i == 6) {
+//     break; }
+//   console.log (i);
+// }
+
+// for (let i = 1; i < 8; i++) {
+//   if (i == 6) {
+//     continue; }
+//     console.log (i);
+// }
+
+// let x = 5; alert( x++ );
+
+// let a = [ ] + 1 + 2;
+// console.log(a);
+
+
+// let y = 1; let x = y = 2; console.log(x);
+
+// alert( "1"[0] )
+
+// let a = 2 && 1 && null && 0 && undefined;
+// console.log(a);
+
+let x = 0 || "" || 2 || undefined || true || falsе;
+console.log(x);
 
