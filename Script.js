d641d1db66402239b9c7ccca72149bc844399408
Maxim <l9sik3@gmail.com>
2021-03-03 21:10:44 +0300

    add cycle

--- Script.js
+++ Script.js
@@ -1,67 +1,37 @@
-// var money = +prompt ('Ваш бюджет на месяц?');
-// var time = +prompt ('Введите дату в формате YYYY-MM-DD');
-// var oneDayBudget;
-// let appData = {
-//   budget:money, //Бюджет
-//   timeData:time, // Данные времени
-//   expenses: {}, //обязательные платежи
-//   optionalExpenses:{},// неоязательные расходы
-//   income:[], //доп.доход
-//   savings:false
-// };
+var money = +prompt ('Ваш бюджет на месяц?');
+var time = +prompt ('Введите дату в формате YYYY-MM-DD');
+var oneDayBudget;
+let appData = {
+  budget:money, //Бюджет
+  timeData:time, // Данные времени
+  expenses: {}, //обязательные платежи
+  optionalExpenses:{},// неоязательные расходы
+  income:[], //доп.доход
+  savings:false
+};
 
-// var a = prompt('Введите обязательную статью расходов в этом месяце'),
-//     b = +prompt('Во сколько обойдется?'),
-//     c = prompt('Введите обязательную статью расходов в этом месяце'),
-//     d = +prompt('Во сколько обойдется?');
+for ( let i = 0; i < 2; i++) {
+  let a = prompt('Введите обязательную статью расходов в этом месяце'),
+      b = +prompt('Во сколько обойдется?');
 
-// appData.expenses [a] = b;
-// appData.expenses [c] = d;
+      if ( (typeof(a) === 'string' && typeof(a) != null && typeof(b) != null && a != '' && b != '' && a.length < 50)) {
+        console.log ('done');
+        appData.expenses[a] = b;
+      } else {
 
-// oneDayBudget = (money - b - d)/30;
+      }
+}
 
-// var x = oneDayBudget;
-// var y = Math.round(oneDayBudget * 10) / 10; // Округление
-// alert ('Ваш дневной бюджет составляет - ' + (y));
+appData.moneyPerDay = appData.budget/30;
 
-// let num = 50;
-// while (num < 55) {
-//   console.log(num);
-//   num++;
-// }
+alert ('Ежедневный бюджет: ' + appData.moneyPerDay);
 
-// // let num = 53;
-// do {
-//   console.log (num);
-//   num++;
-// }
-// while (num < 55);
-
-// for ( let i = 1; i < 8; i++) {
-//   if (i == 6) {
-//     break; }
-//   console.log (i);
-// }
-
-// for (let i = 1; i < 8; i++) {
-//   if (i == 6) {
-//     continue; }
-//     console.log (i);
-// }
-
-// let x = 5; alert( x++ );
-
-// let a = [ ] + 1 + 2;
-// console.log(a);
-
-
-// let y = 1; let x = y = 2; console.log(x);
-
-// alert( "1"[0] )
-
-// let a = 2 && 1 && null && 0 && undefined;
-// console.log(a);
-
-let x = 0 || "" || 2 || undefined || true || falsе;
-console.log(x);
-
+if (appData.moneyPerDay < 100) {
+  console.log('Вы нищеброд!');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log('Вы средний');
+} else if (appData.moneyPerDay > 2000) {
+  console.log('Вы богач!!!');
+} else {
+  console.log ('Mistake!!');
+}
